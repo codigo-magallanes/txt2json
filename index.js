c8eae58df1b5f87d743946b84bc0a60f5f6cb722
codigo-magallanes <jose.media@gmail.com>
2020-10-09 12:39:04 -0300

    fetch docs with promise.all

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import dataOBJ from "./js/dataBase.js";
 import { linesToArray, dataToSunPhases, addNSCDates } from "./sources/astropixels/index.js";
 import { linesToData, dataToMoonPhases } from "./sources/calendario365/index.js";
 
-let astroData = {};
+let astroData = {
+  docs: [],
+  sun: {},
+  moon: {}
+};
+
 let config = {
   /**
    * @values ('json' | 'locale' | 'ms' | 'any' ) Output format of dates if not stablished beforehand
@@ -10,11 +15,8 @@ let config = {
   saveDatesAs: "ms",
 };
 
-/**
- * ** PROMISES **
- */
 
-// GLOBAL
+// GLOBAL FUNCTIONS
 function getTextLines(data) {
   return data.split("\n");
 }
@@ -29,43 +31,54 @@ function showData(data) {
   const elData = document.querySelector("#data");
   elData.innerHTML = JSON.stringify(data, null, 2);
   console.log(data);
+  return data
 }
 
-// NO-DATA
-function noData(source) {
-  let msg = "need some more code to extract data from source.";
-  dataOBJ.sources[source]["error"] = msg;
-}
-
+//promise.then(x => { console.log(x)}).finally(showData)
 // for every source prints dataOBJ in screen.
 // need to fix so it only prints and logs info once
 let source;
+let files = []
 for (source in dataOBJ.sources) {
-  switch (source) {
-    case "astropixels":
-      dataOBJ.sources.astropixels.files.forEach((url) => {
-        fetch(url)
-          .then((res) => res.text())
-          .then(getTextLines)
-          .then(linesToArray)
-          .then(dataToSunPhases)
-          .then(addNSCDates)
-          .then(addData)
-          .then(showData);
-      });
-      break;
-    case "calendario365":
-      dataOBJ.sources.calendario365.files.forEach((url) => {
-        fetch(url)
-          .then((res) => res.text())
-          .then(getTextLines)
-          .then(linesToData)
-          .then(dataToMoonPhases)
-          .then(addData)
-          .then(showData);
-      });
-      break;
-    default:
-      noData(source);
+  let temp = dataOBJ.sources[source]['files']
+  if (temp != undefined) {
+    temp.forEach((url) => files.push(fetch(`${url}`)))
   }
 }
+
+Promise.all(files).
+then(function (responses) {
+  // returns a new array formed with .map()
+  return Promise.all(responses.map(async function (response) {
+    const url = response.url
+    const txt = await response.text();
+    return {
+      url,
+      txt
+    }
+  }));
+}).then(function (arr) {
+  arr.forEach((doc) => {
+    if (doc.url.includes('/astropixels/')) {
+      let lines = getTextLines(doc.txt)
+      let data = linesToArray(lines)
+      let sunData = dataToSunPhases(data)
+      let obj = addNSCDates(sunData)
+      addData(obj)
+    } else if (doc.url.includes('/calendario365/')) {
+      let lines = getTextLines(doc.txt)
+      let data = linesToData(lines)
+      let obj = dataToMoonPhases(data)
+      addData(obj)
+    } else {
+      console.log(`need new code to extract infor from:\n ${doc.url}`)
+    }
+  })
+}).catch(function (error) {
+  // if there's an error, log it
+  console.log(error);
+}).finally(() => {
+  showData({
+    astroData
+  })
+});
