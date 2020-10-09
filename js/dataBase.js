d5339feed8b04491d1b2d13c088a8e9004ab3690
codigo-magallanes <jose.media@gmail.com>
2020-10-09 14:06:12 -0300

    new files url

--- js/dataBase.js
+++ js/dataBase.js
@@ -3,21 +3,21 @@ export default {
     "astropixels": {
       "url": "http://astropixels.com/",
       "files": [
-        "/node/sources/astropixels/sun_2001-2050(equinox-solstice)(astropixels).txt",
-        "/node/sources/astropixels/sun_2051-2100(equinox-solstice)(astropixels).txt"
+        "/sources/astropixels/sun_2001-2050(equinox-solstice)(astropixels).txt",
+        "/sources/astropixels/sun_2051-2100(equinox-solstice)(astropixels).txt"
       ]
     },
     "calendario365": {
       "url": "https://www.calendario-365.es/luna/lunar-fases.html",
       "files": [
-        "/node/sources/calendario365/moon_2020(calendario365).txt",
-        "/node/sources/calendario365/moon_2021(calendario365).txt"
+        "/sources/calendario365/moon_2020(calendario365).txt",
+        "/sources/calendario365/moon_2021(calendario365).txt"
       ]
     },
     "newSource": {
       "url": "http:///example.com",
       "files": [
-        "/node/sources/newsource/example.txt"
+        "/sources/newsource/example.txt"
       ]
     }
   }
